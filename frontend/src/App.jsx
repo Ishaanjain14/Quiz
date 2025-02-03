9dafd536be68012b409ce6a7e7d7e23b87c90745
taniya542 <taniya.srn@gmail.com>
2025-02-03 21:03:21 +0530

    loginpage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
-
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import {InstructionsPage} from "./components/Instructions";
-import {ExamInterface} from "./components/ExamInterface";
+import { InstructionsPage } from "./components/Instructions";
+import { ExamInterface } from "./components/ExamInterface";
+import { LoginPage } from "./components/loginpage";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<InstructionsPage />} />
-        <Route path="/exam" element={<ExamInterface />} />
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/instructions" element={<InstructionsPage />} />
+        <Route path="/exam" element={<ExamInterface/>} />
       </Routes>
     </Router>
   );
